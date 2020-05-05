703cb000924d1c4bddf6a6686f335395297077b1
kiiwii <samcorbitt_@outlook.com>
2020-05-05 12:14:54 -0500

    Multiple - Updated README - Added different file types and file sizes in the script

--- README.md
+++ README.md
@@ -1,6 +1,16 @@
 # Music-Calculator
-This is a way to calculate how big your music collection will be in wavs.
+This is a way to calculate how big your music collection will be in multiple file formats
 
 ## Why are you making this?
 I use an ipod video from 2005 and I'd like to know how big my library will be before I start to put music on it, since I'm upgrading the storage. and I 
 assume others would like to have the ability to check too.
+
+## Usage
+To use it, run main.lua. The script will prompt you with the following quetions:
+- How many songs are in your library?
+- What file type are your songs?
+- What file size? (mb or gb)
+The input for file type and file size MUST be lowercase. If not, the script will not run correctly.
+
+After answering all the questions, you will get a minimum, maximum and average overall file size for all the songs. This is useful if you haven't 
+ripped/downloaded the songs and would like to know how much space it'll take up.
--- main.lua
+++ main.lua
@@ -1,7 +1,6 @@
 --[[
-This is my calculator to see how big your music library will be in the WAV 
-format. The WAV format is a uncompressed, lossless audio type. It's basically
-like your listening straight from the CD itself.
+This is a lua script that will help you find out how big your music library will
+be in multiple file formats. Please refer to the README for more information.
 
 If you do modify it as your own application, please keep this comment block, so
 people can know I am the original author. Thank you :)
@@ -10,17 +9,76 @@ Original Author: Kiiwii
 Made in: May 2020
 --]]
 
+local filetypes={
+	['mp3'] = function(x) return x end,
+	['wav'] = function(x) return x end
+}
+
+local size={
+	['mb'] = function(x) return x end,
+	['gb'] = function(x) return x end
+}
+
+local function errorfile()
+	print "Unknown file type!"
+end
+
+local function errorsize()
+	print "Unknown file size!"
+end
+
 print('How many songs are in your library?')
 local num1 = io.read()
 
--- This will hold the minimum, maximum and average of the file size in megabytes.
-local min = num1*10
-local max = num1*40
-local avg = (min+max)/2
+print('What file type your songs? (mp3 or wav)')
+local file = filetypes[io.read()] or errorfile
 
--- This will print the min, max, and avg of the music library in megabytes
-print('Minimum size: ', min,'mb')
-print('Maximum size: ', max,'mb')
-print('Average size: ', avg,'mb')
+print('What file size? (mb or gb)')
+local sizes = size[io.read()] or errorsize
 
+-- MP3 min, max and avg at 128kbps in MB
+local mp3minmb = file(num1*1)
+local mp3maxmb = file(num1*3)
+local mp3avgmb = file((mp3minmb+mp3maxmb)/2)
+
+-- MP3 min, max and avg at 128kbps in GB
+local mp3mingb = mp3minmb/1024
+local mp3maxgb = mp3maxmb/1024
+local mp3avggb = mp3avgmb/1024
+
+-- WAV min, max and avg in MB
+local wavminmb = file(num1*10)
+local wavmaxmb = file(num1*40)
+local wavavgmb = file((wavminmb+wavmaxmb)/2)
+
+-- WAV min, max and avg in GB
+local wavmingb = wavminmb/1024
+local wavmaxgb = wavmaxmb/1024
+local wavavggb = wavavgmb/1024
+
+-- Calculates sizes in MB
+if file == filetypes.mp3 and sizes == size.mb then
+	print('MP3 Minimum size: ', mp3minmb,'mb')
+	print('MP3 Maximum size: ', mp3maxmb,'mb')
+	print('MP3 Average size: ', mp3avgmb,'mb')
+elseif file == filetypes.wav and sizes == size.mb then
+	print('WAV Minimum size: ', wavminmb,'mb')
+	print('WAV Maximum size: ', wavmaxmb,'mb')
+	print('WAV Average size: ', wavavgmb,'mb')
+end
+
+-- Calculates sizes in GB
+if file == filetypes.mp3 and sizes == size.gb then
+	print('MP3 Minimum size: ', mp3mingb,'gb')
+	print('MP3 Maximum size: ', mp3maxgb,'gb')
+	print('MP3 Average size: ', mp3avggb,'gb')
+elseif file == filetypes.wav and sizes == size.gb then
+	print('WAV Minimum size: ', wavmingb,'gb')
+	print('WAV Maximum size: ', wavmaxgb,'gb')
+	print('WAV Average size: ', wavavggb,'gb')
+end
+
+print('Press ENTER to quit.')
+
+io.read()
 os.exit()
